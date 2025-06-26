aebb6cb9d060f416916a56c829064c44487b20aa
Mukul Gupta <130096732+mukulgupta11@users.noreply.github.com>
2025-06-26 13:21:04 +0530

    Update apiPaths.js

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://project-management-uwiv.onrender.com";
 // export const BASE_URL = "https://task-manager-backend-auir.onrender.com";
 
 // utils/apiPaths.js
